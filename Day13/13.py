2d3010e4691395c737fddbeb06ab5ce0f7b99a31
stgasser <stefandotgasser@gmail.com>
2020-12-16 23:12:39 +0100

    Update 13.py
    Added comments
    fixed spelling

--- Day13/13.py
+++ Day13/13.py
@@ -4,24 +4,27 @@ def parse_input(filename):
         return int(lines[0]), [(i, int(b)) for i, b in enumerate(lines[1].split(',')) if b != 'x']
 
 
-def part1(timestamp, busses):
-    _, b = min(busses, key=lambda bus: (bus[1] - timestamp % bus[1]))
+def part1(timestamp, buses):
+    # find bus with minmal delay
+    _, b = min(buses, key=lambda bus: (bus[1] - timestamp % bus[1]))
     return b * (b - timestamp % b)
 
 
-def part2(timestamp, busses):
+def part2(timestamp, buses):
     curr = 0
     found = 0
     adv = 1
-    dt, bus = busses[found]
+    dt, bus = buses[found]
     while True:
+        # only check timestamps where all previous buses arrive at the correct delay
         curr += adv
         if (curr + dt) % bus == 0:
+            # the all the already aligned buses will align again in the product of their busid's
             adv *= bus
             found += 1
-            if found == len(busses):
+            if found == len(buses):
                 break
-            dt, bus = busses[found]
+            dt, bus = buses[found]
     return curr
 
 
